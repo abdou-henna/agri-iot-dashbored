35b342e8e3dcf4b47a622def8944d77132892e5c
Abderrahmane Henna <henna1abdou@gmail.com>
2026-05-02 01:48:30 +0100

    Remove embedded WebService before subtree setup

--- WebService/src/routes/nodes.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getNodes } from '../controllers/nodes.controller.js';
-
-const router = express.Router();
-
-// GET /api/v1/nodes
-router.get('/', getNodes);
-
-export default router;
--- WebService/src/routes/status.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getStatus } from '../controllers/status.controller.js';
-
-const router = express.Router();
-
-// GET /api/v1/status
-router.get('/', getStatus);
-
-export default router;
